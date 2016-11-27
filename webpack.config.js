f04bcc66a63bdae45fa0c607d80866a0f97eaeb9
Tomas Zaltauskas <tomaszaltauskas@gmail.com>
2016-11-28 01:44:04 +0200

    minify optimization added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+var webpack = require('webpack');
+var minify = process.env.minify === "true";
+
 module.exports = {
     entry: "./required-modules.js",
     output: {
         path: "./app/generated",
         filename: "merged-modules.js"
-    }
+    },
+    plugins: minify ? [new webpack.optimize.UglifyJsPlugin()] : []
 };
