d8c5fe31e9e3466a4d59042f846465f2e4866bb0
Tomas Zaltauskas <tomaszaltauskas@gmail.com>
2016-11-28 00:41:21 +0200

    webpack added

---
+++ required-modules.js
@@ -0,0 +1,2 @@
+require('./app/app.js');
+require('./app/view1/view1.js');
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: "./required-modules.js",
+    output: {
+        path: "./app/generated",
+        filename: "merged-modules.js"
+    }
+};
